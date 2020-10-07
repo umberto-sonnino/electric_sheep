ff7fa29960d0a79e8b27de59cec9ca03c9dffaaa
Umberto Sonnino <usonnino@gmail.com>
2020-10-07 16:50:14 +0100

    Improved layout & cardview

--- app/src/main/java/com/rive/electric_sheep/LoadActivity.kt
+++ app/src/main/java/com/rive/electric_sheep/LoadActivity.kt
@@ -2,14 +2,18 @@ package com.rive.electric_sheep
 
 import android.animation.ArgbEvaluator
 import android.animation.ValueAnimator
+import android.content.Context
 import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.text.Layout.JUSTIFICATION_MODE_INTER_WORD
 import android.view.Gravity
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
 import android.widget.LinearLayout
+import android.widget.ListView
 import android.widget.Switch
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 interface LoadFinishedDelegate {
@@ -17,10 +21,10 @@ interface LoadFinishedDelegate {
 }
 
 class LoadActivity : AppCompatActivity(), LoadFinishedDelegate {
-    lateinit var switch: Switch
-    lateinit var animationView: ElectricSheep
-    lateinit var loremIpsum: TextView
-    lateinit var layout: LinearLayout
+    private lateinit var switch: Switch
+    private lateinit var animationView: ElectricSheep
+    private lateinit var listView: ListView
+    private lateinit var layout: LinearLayout
 
     val bgColor = Color.parseColor("#3497DB")
 
@@ -30,28 +34,19 @@ class LoadActivity : AppCompatActivity(), LoadFinishedDelegate {
         layout = LinearLayout(this);
         layout.orientation = LinearLayout.VERTICAL
 
+        val pad = 50
+        layout.setPadding(pad, pad * 2, pad, 0)
+
         // Add the Sheep Animation
-        val android_sheep = resources.openRawResource(R.raw.android_sheep)
-        val bytes = android_sheep.readBytes()
+        val androidSheep = resources.openRawResource(R.raw.android_sheep)
+        val bytes = androidSheep.readBytes()
         animationView = ElectricSheep(bytes, this)
         animationView.layoutParams = LinearLayout.LayoutParams(
             LinearLayout.LayoutParams.MATCH_PARENT,
             1200
         )
 
-        loremIpsum = TextView(this)
-        loremIpsum.setText(R.string.lorem_ipsum)
-        loremIpsum.visibility = View.GONE
-        val textParams = LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.WRAP_CONTENT,
-            LinearLayout.LayoutParams.WRAP_CONTENT
-        )
-        textParams.setMargins(30, 30, 30, 0)
-        loremIpsum.layoutParams = textParams
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            loremIpsum.justificationMode = JUSTIFICATION_MODE_INTER_WORD
-        }
-
+        // Setup Switch that toggles animation visibility.
         switch = Switch(this)
         val switchParams = LinearLayout.LayoutParams(
             LinearLayout.LayoutParams.WRAP_CONTENT,
@@ -61,9 +56,18 @@ class LoadActivity : AppCompatActivity(), LoadFinishedDelegate {
         switchParams.setMargins(30, 100, 30, 30)
         switch.layoutParams = switchParams
 
+        // Setup ListView that'll become visible at the end of the animation.
+        listView = ListView(this)
+        val cards = Array<String>(10) { "$it" }
+        listView.adapter = CardAdapter(this, cards)
+        val divider = ColorDrawable(Color.TRANSPARENT)
+        listView.divider = divider
+        listView.dividerHeight = 50
+        listView.visibility = View.GONE
 
+        // Add all the elements to the screen.
         layout.addView(animationView);
-        layout.addView(loremIpsum)
+        layout.addView(listView)
         layout.addView(switch)
         layout.setBackgroundColor(bgColor)
         setContentView(layout)
@@ -73,18 +77,22 @@ class LoadActivity : AppCompatActivity(), LoadFinishedDelegate {
         }
     }
 
+    fun toggleFinished(isChecked: Boolean) {
+        if (isChecked) {
+            animationView.onComplete()
+        }
+    }
+
     override fun onLoadingFinished() {
+        hideAnimation()
+        showList()
+    }
+
+    private fun hideAnimation() {
         switch.visibility = View.GONE
         animationView.visibility = View.GONE
-        loremIpsum.apply {
-            alpha = 0f
-            visibility = View.VISIBLE
-            animate()
-                .alpha(1f)
-                .setDuration(1500)
-                .setListener(null)
-        }
-        val colorAnimation = ValueAnimator.ofObject(ArgbEvaluator(), bgColor, Color.WHITE)
+
+        val colorAnimation = ValueAnimator.ofObject(ArgbEvaluator(), bgColor, R.color.purple)
         colorAnimation.duration = 250
         colorAnimation.addUpdateListener {
             layout.setBackgroundColor(it.getAnimatedValue() as Int)
@@ -92,9 +100,41 @@ class LoadActivity : AppCompatActivity(), LoadFinishedDelegate {
         colorAnimation.start()
     }
 
-    fun toggleFinished(isChecked: Boolean) {
-        if (isChecked) {
-            animationView.onComplete()
+    private fun showList() {
+        listView.apply {
+            alpha = 0f
+            visibility = View.VISIBLE
+            animate()
+                .alpha(1f)
+                .setDuration(1500)
+                .setListener(null)
         }
+    }
+
+}
+
+class CardAdapter(
+    private val context: Context,
+    private val dataSource: Array<String>
+) : BaseAdapter() {
+
+    private val inflater: LayoutInflater =
+        context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+    override fun getCount(): Int {
+        return dataSource.size
+    }
+
+    override fun getItem(p0: Int): Any {
+        return dataSource[p0]
+    }
+
+    override fun getItemId(p0: Int): Long {
+        return p0.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val rowView = inflater.inflate(R.layout.list_row, parent, false)
+        return rowView
     }
 }
